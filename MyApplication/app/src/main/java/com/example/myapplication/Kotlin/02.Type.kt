41e6121e166b510d2c6fbe3f05b8a6339ade75a7
DoHoon <samdo0812@gmail.com>
2020-03-24 22:30:15 +0900

    Kotlin Type

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Kotlin/02.Type.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.Kotlin
+
+//02. 자료형
+//정수형 : long int byte short
+//실수형 : double float
+//문자형 : char
+//문자열 : String
+//논리형 : boolean
+
+var number = 10
+
+//명시적 선언
+var number1 : Int = 20
+var hello1 : String = "Hello"
+var point1 : Double = 3.4
+
+//variable : value
+//1. 변하지 않는 값 value
+
+fun main(array: Array<String>){
+    number = 20
+    //number = 20.5
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/Kotlin/03.Practice.kt
@@ -0,0 +1,22 @@
+package com.example.myapplication.Kotlin
+
+var a = "1"
+var b = a.toInt()
+var c = b.toFloat()
+
+var d = "John"
+var e = "My name is $d Nice to meet you"
+
+//Null
+//var abc : Int = null
+var abc1 : Int? = null
+var abc2 : Double? = null
+
+fun main(array: Array<String> ){
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(abc1)
+}

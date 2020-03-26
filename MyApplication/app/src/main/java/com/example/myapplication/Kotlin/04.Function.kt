7cb34ff9b3e4f51b67d48fe4d043bf56bf53b0fd
DoHoon <samdo0812@gmail.com>
2020-03-26 22:32:27 +0900

    Kotlin function

--- MyApplication/app/src/main/java/com/example/myapplication/Kotlin/04.Function.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Kotlin/04.Function.kt
@@ -7,13 +7,40 @@ package com.example.myapplication.Kotlin
 //  return 반환값
 //}
 
-fun plus(first: Int, second: Int): Int{
+fun plus(first: Int, second: Int): Int {
     val result: Int = first + second
     return result
 }
 
-fun main(array: Array<String>){
-    val result = plus(5,10)
+//default 값을 갖는 함
+fun plusFive(first: Int, second: Int = 5): Int {
+    val result: Int = first + second
+    return result
+}
+
+//반환값이 없는 함수, Unit 생략가
+fun printPlus(first: Int, second: Int): Unit {
+    val result: Int = first + second
+    println(result)
+}
+
+fun printPlusTwo(first: Int, second: Int) {
+    val result: Int = first + second
+    println(result)
+}
+
+//간단하게 함수선언
+fun printShort(first: Int, second: Int) = first + second
+
+//가변인자 함수
+fun plusMany(vararg numbers: Int){
+    for(number in numbers){
+        println(number)
+    }
+}
+
+fun main(array: Array<String>) {
+    val result = plus(5, 10)
     println(result)
 
     //인수를 명시적으로 전달
@@ -22,5 +49,19 @@ fun main(array: Array<String>){
     val result3 = plus(second = 20, first = 10)
     println(result3)
 
-    
+    println()
+    val result4 = plusFive(10, 20)
+    println(result4)
+    val result5 = plusFive(10)
+    println(result5)
+
+    println()
+    printPlus(10, 20)
+
+    val result6 = printShort(50, 50)
+    println(result6)
+
+    println()
+    plusMany(1,2,3)
+    plusMany(100)
 }

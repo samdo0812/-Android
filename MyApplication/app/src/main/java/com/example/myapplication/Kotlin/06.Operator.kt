3bb94652213303ec8f099618b628518abc945c0b
DoHoon <samdo0812@gmail.com>
2020-03-31 22:45:43 +0900

    Kotlin operator

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Kotlin/06.Operator.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.Kotlin
+
+//연산자
+
+//산술 연산자
+//+ - * / %
+
+//대입 연산자
+//좌변 = 우변(우변값이 좌변값으로)
+
+//복합 대입 연산자
+//+= -= *= /= %=
+
+//증감 연산자
+//++ --
+
+//비교 연산자
+//> < >= <= == !=
+//True == True -> true
+//True == False -> false
+//True != True -> false
+//True != False -> true
+
+//논리 연산자
+//&& || !

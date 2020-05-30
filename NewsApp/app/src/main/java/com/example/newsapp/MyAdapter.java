03165f8ac4febb5a42e18d1420e1489699ef7884
DoHoon <samdo0812@gmail.com>
2020-05-30 23:22:21 +0900

    NewsApp

--- NewsApp/app/src/main/java/com/example/newsapp/MyAdapter.java
+++ NewsApp/app/src/main/java/com/example/newsapp/MyAdapter.java
@@ -1,5 +1,7 @@
 package com.example.newsapp;
 
+import android.content.Context;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,8 +11,13 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
-    private String[] mDataset;
+    private List<NewsData>  mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
@@ -19,18 +26,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         // each data item is just a string in this c ase
         public TextView TextView_title;
         public TextView TextView_Content;
-        public ImageView ImageView_title;
+        public SimpleDraweeView ImageView_title;
         public MyViewHolder(View v) {
             super(v);
             TextView_title = v.findViewById(R.id.TextView_title);
-            TextView_Content = v.findViewById(R.id.TextView_content);
-            ImageView_title = v.findViewById(R.id.ImageView_title);
+            TextView_Content = v.findViewById(R.id.TextView_Content);
+            ImageView_title = (SimpleDraweeView) v.findViewById(R.id.ImageView_title);
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(List<NewsData> myDataset, Context context) {
         mDataset = myDataset;
+        Fresco.initialize(context);
     }
 
     // Create new views (invoked by the layout manager)
@@ -50,13 +58,26 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     public void onBindViewHolder(MyViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.TextView_title .setText(mDataset[position]);
+        NewsData news = mDataset.get(position);
+
+        holder.TextView_title .setText(news.getTitle());
+
+        String content = news.getContent();
+        if(content != null && content.length() > 0) {
+            holder.TextView_Content.setText(content);
+        }
+        else {
+            holder.TextView_Content.setText("-");
+        }
+
+        Uri uri = Uri.parse(news.getUrlToImage());
+        holder.ImageView_title.setImageURI(uri);
 
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset == null ? 0 :  mDataset.size();
     }
 }
--- NewsApp/app/src/main/java/com/example/newsapp/NewsActivity.java
+++ NewsApp/app/src/main/java/com/example/newsapp/NewsActivity.java
@@ -5,6 +5,21 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsActivity extends AppCompatActivity {
 
@@ -12,11 +27,11 @@ public class NewsActivity extends AppCompatActivity {
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
     private String[] myDataset={"1","2"};
-
+    RequestQueue queue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_news );
+        setContentView(R.layout.activity_news);
         recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
         // use this setting to improve performance if you know that changes
@@ -27,8 +42,63 @@ public class NewsActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(myDataset);
-        recyclerView.setAdapter(mAdapter);
+
+        queue = Volley.newRequestQueue(this);
+        getNews();
+        //1.화면로딩 -> 뉴스정보를 받아온다.
+        //2.정보 -> 어댑터로 넘겨준다.
+        //3.어댑터 -> 세팅
+    }
+
+    public void getNews(){
+        // Instantiate the RequestQueue.
+        String url ="key";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        //Log.d("NEWS",response);
+
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            JSONArray arrayArticles = jsonObj.getJSONArray( "articles");
+
+                            //response -> NewsData class 분류
+                            List<NewsData> news = new  ArrayList<>();
+
+                            for (int i=0, j=arrayArticles.length(); i<j; i++){
+                                JSONObject obj = arrayArticles.getJSONObject(i);
+
+                                Log.d("NEWS",obj.toString());
+
+                                NewsData newsData = new NewsData();
+                                newsData.setTitle(obj.getString("title"));
+                                newsData.setUrlToImage(obj.getString("urlToImage"));
+                                newsData.setContent(obj.getString("description"));
+                                news.add(newsData);
+
+                            }
+                            // specify an adapter (see also next example)
+                            mAdapter = new MyAdapter(news, NewsActivity.this);
+                            recyclerView.setAdapter(mAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+            }
+        });
+
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }

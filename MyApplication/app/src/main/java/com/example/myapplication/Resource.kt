dff1c5443acd453057e89987790473df080c4889
DoHoon <samdo0812@gmail.com>
2020-06-25 21:58:51 +0900

    Resource

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Resource.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_resource.*
+
+class Resource : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resource)
+
+        //1
+        val ment = resources.getString(R.string.hello)
+        Log.d("mentt", "ment : " + ment)
+
+        //2
+        val ment2 = getString(R.string.hello)
+        Log.d("mentt", "ment : " + ment2)
+
+        //3
+        val color = getColor(R.color.textview_color)
+        Log.d("ment", "color : " + color)
+
+        button.setBackgroundColor(getColor(R.color.textview_color))
+    }
+}

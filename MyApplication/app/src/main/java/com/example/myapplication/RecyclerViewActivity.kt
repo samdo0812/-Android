2eafec861a91fb0f9c5a71b5b55f21445e9967e0
DoHoon <samdo0812@gmail.com>
2020-07-19 16:21:45 +0900

    recyclerView

---
+++ MyApplication/app/src/main/java/com/example/myapplication/RecyclerViewActivity.kt
@@ -0,0 +1,54 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_recycler_view.*
+
+class RecyclerViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+
+
+        val carList = ArrayList<CarForList>()
+        for (i in 0 until 50) {
+            carList.add(CarForList("" + i + " 번째 자동차", "" + i + "순위 엔진"))
+        }
+        val adapter = RecyclerViewAdapter(carList, LayoutInflater.from(this))
+        recycler_view.adapter = adapter
+        recycler_view.layoutManager = LinearLayoutManager(this)
+
+    }
+}
+
+class RecyclerViewAdapter(val itemList:ArrayList<CarForList>, val inflater:LayoutInflater): RecyclerView.Adapter<RecyclerViewAdapter.viewHolder>(){
+    class viewHolder(itemView:View):RecyclerView.ViewHolder(itemView){
+        val carName: TextView
+        val carEngine: TextView
+        init {
+            carName = itemView.findViewById(R.id.car_name)
+            carEngine = itemView.findViewById(R.id.car_engine )
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): viewHolder {
+        val view = inflater.inflate(R.layout.item_view,parent,false)
+        return viewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return itemList.size
+    }
+
+    override fun onBindViewHolder(holder: viewHolder, position: Int) {
+        holder.carName.setText(itemList.get(position).name)
+        holder.carEngine.setText(itemList.get(position).engine)
+    }
+}
